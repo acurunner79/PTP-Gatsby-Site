3cd339331694d760128e153ed7b3d8d67899faeb
Jorge <djsmoothzouk@gmail.com>
2021-08-03 11:54:50 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "PTP-Gatsby-Contentful",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+  ],
+};
