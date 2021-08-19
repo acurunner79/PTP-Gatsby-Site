4acfea3e3d0ad6e353e8faaab0ec6a604d9360b6
Jorge <djsmoothzouk@gmail.com>
2021-08-19 15:06:30 -0500

    added the SEO component and all metaData

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,12 +38,12 @@ module.exports = {
         maxVideos: 50 // Defaults to 50
       },
     },
-    {
-      resolve: 'gatsby-plugin-mailchimp',
-      options: {
-          endpoint: '', // string; add your MC list endpoint here; see instructions below
-          timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
-      },
-    },
+    // {
+    //   resolve: 'gatsby-plugin-mailchimp',
+    //   options: {
+    //       endpoint: '', // string; add your MC list endpoint here; see instructions below
+    //       timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
+    //   },
+    // },
   ],
 };
