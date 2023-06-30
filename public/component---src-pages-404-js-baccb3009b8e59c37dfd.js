4b90f04f0d67f89e0a6efc4ebe7068438077cfe8
Jorge <jorge@pop-os.localdomain>
2023-06-29 20:00:25 -0500

    Added Google GTAG plugin and configured for Google Analytics

---
+++ public/component---src-pages-404-js-baccb3009b8e59c37dfd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkptp_gatsby_contentful=self.webpackChunkptp_gatsby_contentful||[]).push([[883],{429:function(e,t,n){n.r(t);var l=n(7294),u=n(1883),a=n(3571),o=n(188);t.default=()=>l.createElement(a.Z,null,l.createElement(o.Z,{title:"404: Not found"}),l.createElement("h1",null,"404: Oops! Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."),l.createElement(u.Link,{to:"/"},"Go home"))}}]);
+//# sourceMappingURL=component---src-pages-404-js-baccb3009b8e59c37dfd.js.map
