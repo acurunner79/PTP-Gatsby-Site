b06f570db1f150421faf4c77e42bdc847a8ed23a
Jorge <jorge@pop-os.localdomain>
2023-06-30 11:36:48 -0500

    Added Google GTAG plugin and configured for Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-RFWM3P0XZ8", // Google Analytics / GA
+          "G-RFWM3P0XZ8",
         ],
         // This object is used for configuration specific to this plugin
         pluginConfig: {
